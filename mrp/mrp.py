7b1c2bfa231c4209ccd8065aa5e4858556e5da47
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2024-04-24 21:36:44 +0200

    update 2

--- mrp/mrp.py
+++ mrp/mrp.py
@@ -16,27 +16,35 @@ class MRP:
 
     def calculate_mrp(self):
         for product in self.products:
-            print(f"Produkt: {product.nazwa_produktu}")
-            for i in range(1, 6):
-                print("Tydzien", i)
-                potrzeby_brutto = 0 if i < product.tydzien_na_ktory_chcemy_produkty else product.ilosc_produktow_na_wskazany_tydzien
-                wstepny_zapas = product.zapas if i <= product.tydzien_na_ktory_chcemy_produkty else 0
-                potrzeby_netto = 0 if potrzeby_brutto - wstepny_zapas < 0 else potrzeby_brutto - wstepny_zapas
-                zmontowanie = (product.ilosc_produktow_na_wskazany_tydzien - product.zapas) if (i == product.tydzien_na_ktory_chcemy_produkty - product.czas_produkcji_elementow) else 0
-                odbior = 0 if i < product.tydzien_na_ktory_chcemy_produkty else potrzeby_netto
-                print("Potrzeby brutto:", potrzeby_brutto)
-                print("Wstępny zapas:", wstepny_zapas)
-                print("Potrzeby netto:", potrzeby_netto)
-                print("Wstępne zmontowanie:", zmontowanie)
-                print("Zaplanowany odbior:", odbior)
+            if product.level == 0:
+                print(f"Produkt: {product.nazwa_produktu}")
+                for i in range(1, 9):
+                    print("Tydzien", i)
+                    potrzeby_brutto = 0 if i < product.tydzien_na_ktory_chcemy_produkty else product.ilosc_produktow_na_wskazany_tydzien
+                    wstepny_zapas = product.zapas if i <= product.tydzien_na_ktory_chcemy_produkty else 0
+                    potrzeby_netto = max(0, potrzeby_brutto - wstepny_zapas)
+                    zmontowanie = product.ilosc_produktow_na_wskazany_tydzien - product.zapas if (i == product.tydzien_na_ktory_chcemy_produkty - product.czas_produkcji_elementow) else 0
+                    odbior = potrzeby_netto if i >= product.tydzien_na_ktory_chcemy_produkty else 0
+                    if i > product.tydzien_na_ktory_chcemy_produkty:
+                        potrzeby_brutto = 0
+                        wstepny_zapas = 0
+                        potrzeby_netto = 0
+                        zmontowanie = 0
+                        odbior = 0
+                    print("Potrzeby brutto:", potrzeby_brutto, "Wstępny zapas:", wstepny_zapas, "Potrzeby netto:", potrzeby_netto, "Wstępne zmontowanie:", zmontowanie, "Zaplanowany odbior:", odbior)
+                    print("\n")
 
+            if product.level == 1:
+                pass # na razie tak, trzeba zrobic jeszcze level 2 i mozna ulepszac
 
-# Przykładowe użycie
 if __name__ == "__main__":
     mrp_system = MRP()
 
-    stoly = Product("Stoly", 0, 1, 2, 20,5)
+    stoly = Product("Stoly", 0, 1, 2, 20, 5)
+    nogi = Product("Nogi", 1, 2, 40, 80, 5)
+
     mrp_system.add_product(stoly)
+    mrp_system.add_product(nogi)
 
     mrp_system.calculate_mrp()
 
